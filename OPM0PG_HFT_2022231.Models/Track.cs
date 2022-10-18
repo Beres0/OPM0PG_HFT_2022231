0d1820bc9373e87ff1148a36f9ad1b74962cd3b0
beresB <57649310+beresB@users.noreply.github.com>
2022-10-18 18:35:15 +0200

    Track hozzáadva.

---
+++ OPM0PG_HFT_2022231.Models/Track.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace OPM0PG_HFT_2022231.Models
+{
+    [Index(nameof(No), nameof(AlbumId), IsUnique = true)]
+    public class Track : IEntity<int>
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+
+        public int No { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        [JsonIgnore]
+        public virtual Album Album { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Like> Likes { get; set; }
+    }
+}
