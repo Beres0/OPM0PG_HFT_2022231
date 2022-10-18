bd30844678651a413e20b0d224908141ad76c07e
beresB <57649310+beresB@users.noreply.github.com>
2022-10-18 18:14:58 +0200

    Contributor hozzáadva.

---
+++ OPM0PG_HFT_2022231.Models/Contributor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace OPM0PG_HFT_2022231.Models
+{
+    public class Contributor
+    {
+        [Key, Column(Order = 0), ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+
+        [Key, Column(Order = 1), ForeignKey(nameof(Artist))]
+        public int ArtistId { get; set; }
+
+        [JsonIgnore]
+        public virtual Album Album { get; set; }
+
+        [JsonIgnore]
+        public virtual Artist Artist { get; set; }
+    }
+}
