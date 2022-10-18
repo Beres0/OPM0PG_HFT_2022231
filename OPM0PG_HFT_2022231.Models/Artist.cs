9b55cfc3d41c25b0e0c840282204195ba59a2bf7
beresB <57649310+beresB@users.noreply.github.com>
2022-10-18 18:14:42 +0200

    Artist módosítva.

--- OPM0PG_HFT_2022231.Models/Artist.cs
+++ OPM0PG_HFT_2022231.Models/Artist.cs
@@ -1,21 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace OPM0PG_HFT_2022231.Models
 {
-    public class Artist:IEntity<int>
+    public class Artist : IEntity<int>
     {
-        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [StringLength(255), Required]
         public string Name { get; set; }
 
-        [ForeignKey(nameof(Members))]
+        [ForeignKey(nameof(Band))]
         public int? BandId { get; set; }
+
+        [JsonIgnore]
+        public virtual Artist Band { get; set; }
+
+        [JsonIgnore, InverseProperty(nameof(Band))]
         public virtual ICollection<Artist> Members { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<Contributor> ContributedAlbums { get; set; }
     }
 }
