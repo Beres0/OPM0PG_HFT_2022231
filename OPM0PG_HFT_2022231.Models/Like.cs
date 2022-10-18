f05ab9d25544b9da2a7996f75cb87487378d8502
beresB <57649310+beresB@users.noreply.github.com>
2022-10-18 18:16:06 +0200

    Like hozzáadva.

---
+++ OPM0PG_HFT_2022231.Models/Like.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace OPM0PG_HFT_2022231.Models
+{
+    public class Like : IEntity<object>
+    {
+        [Key, Column(Order = 0), ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+
+        [Key, Column(Order = 1), ForeignKey(nameof(Song))]
+        public int SongId { get; set; }
+
+        [JsonIgnore]
+        public virtual User User { get; set; }
+
+        [JsonIgnore]
+        public virtual Track Song { get; set; }
+
+        [NotMapped, JsonIgnore]
+        object IEntity<object>.Id => new { UserId, SongId };
+    }
+}
