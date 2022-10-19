15cb147b49efb2c2bc3516d121feba5229af2c99
beresB <57649310+beresB@users.noreply.github.com>
2022-10-19 11:59:07 +0200

    Repository módosítva.

--- OPM0PG_HFT_2022231.Repository/Repository.cs
+++ OPM0PG_HFT_2022231.Repository/Repository.cs
@@ -1,21 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OPM0PG_HFT_2022231.Models;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace OPM0PG_HFT_2022231.Repository
 {
-    internal class Repository<TKey, TEntity> : IRepository<TKey, TEntity>
-        where TEntity :class, IEntity<TKey>
+    public class Repository<TKey, TEntity> : IRepository<TKey, TEntity>
+        where TEntity : class, IEntity<TKey>
     {
-        DbContext context;
+        private DbContext context;
+
         public Repository(DbContext context)
         {
             this.context = context;
         }
+
         public void Create(TEntity item)
         {
             context.Set<TEntity>().Add(item);
@@ -24,7 +25,7 @@ namespace OPM0PG_HFT_2022231.Repository
 
         public void Delete(TKey id)
         {
-            context.Remove(Read(id));
+            context.Set<TEntity>().Remove(Read(id));
             context.SaveChanges();
         }
 
@@ -37,11 +38,39 @@ namespace OPM0PG_HFT_2022231.Repository
         {
             return context.Set<TEntity>();
         }
-        
+
         public void Update(TEntity item)
         {
-            Read(item.Id);
+            TEntity old = Read(item.Id);
+            for (int i = 0; i < updaters.Length; i++)
+            {
+                updaters[i](item, old);
+            }
+
             context.SaveChanges();
+        }
+
+        private static Action<TEntity, TEntity>[] updaters = CreateUpdaters();
+
+        private static Action<TEntity, TEntity>[] CreateUpdaters()
+        {
+            Type type = typeof(TEntity);
+            PropertyInfo[] props = type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.CanWrite && !p.SetMethod.IsVirtual).ToArray();
+
+            Action<TEntity, TEntity>[] updaters = new Action<TEntity, TEntity>[props.Length];
+
+            var from = Expression.Parameter(type, "from");
+            var to = Expression.Parameter(type, "to");
+
+            for (int i = 0; i < props.Length; i++)
+            {
+                updaters[i] = Expression.Lambda<Action<TEntity, TEntity>>(
+                    Expression.Assign(
+                        Expression.Property(to, props[i].Name), Expression.Property(from, props[i].Name))
+                    , from, to).Compile();
+            }
+            return updaters;
         }
     }
 }
