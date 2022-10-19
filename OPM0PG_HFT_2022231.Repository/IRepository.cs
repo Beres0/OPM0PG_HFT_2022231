6cfb4741510b1c6341cdf6f28f150135e1459e14
beresB <57649310+beresB@users.noreply.github.com>
2022-10-19 12:00:19 +0200

    IRepository módosítva.

--- OPM0PG_HFT_2022231.Repository/IRepository.cs
+++ OPM0PG_HFT_2022231.Repository/IRepository.cs
@@ -1,15 +1,18 @@
 ﻿using OPM0PG_HFT_2022231.Models;
-using System;
 using System.Linq;
 
 namespace OPM0PG_HFT_2022231.Repository
 {
-    public interface IRepository<TKey,TEntity> where TEntity : IEntity<TKey>
+    public interface IRepository<TKey, TEntity> where TEntity : IEntity<TKey>
     {
         void Create(TEntity item);
+
         IQueryable<TEntity> ReadAll();
+
         TEntity Read(TKey id);
+
         void Update(TEntity item);
+
         void Delete(TKey id);
     }
 }
