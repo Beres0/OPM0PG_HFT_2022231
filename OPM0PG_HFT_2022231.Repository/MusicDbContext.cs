ec92a409a8f157721f8c5d9a12f16b3a5264c349
beresB <57649310+beresB@users.noreply.github.com>
2022-10-23 19:06:56 +0200

    Context módosítva.

--- OPM0PG_HFT_2022231.Repository/MusicDbContext.cs
+++ OPM0PG_HFT_2022231.Repository/MusicDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OPM0PG_HFT_2022231.Models;
-using System;
+using OPM0PG_HFT_2022231.Repository.Configuration;
 
 namespace OPM0PG_HFT_2022231.Repository
 {
@@ -9,11 +9,12 @@ namespace OPM0PG_HFT_2022231.Repository
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
-        public DbSet<Member> Members { get; set; }
         public DbSet<Contributor> Contributors { get; set; }
         public DbSet<Track> Tracks { get; set; }
+        public DbSet<Member> Members { get; set; }
         public DbSet<Part> Parts { get; set; }
         public DbSet<Release> Releases { get; set; }
+
         public MusicDbContext()
         {
             Database.EnsureCreated();
@@ -22,13 +23,19 @@ namespace OPM0PG_HFT_2022231.Repository
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLazyLoadingProxies();
-            string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Beres\\OneDrive\\OE-UMI\\repos\\3_felev\\Halfej\\OPM0PG_HFT_2022231\\OPM0PG_HFT_2022231.Repository\\Database\\MusicDb.mdf;Integrated Security=True;MultipleActiveResultSets=True";
-            optionsBuilder.UseSqlServer(connStr);
+            optionsBuilder.UseInMemoryDatabase("MusicDb");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.ApplyConfiguration(new AlbumConfiguration());
+            modelBuilder.ApplyConfiguration(new ArtistConfiguration());
+            modelBuilder.ApplyConfiguration(new ContributorConfiguration());
+            modelBuilder.ApplyConfiguration(new GenreConfiguration());
+            modelBuilder.ApplyConfiguration(new MemberConfiguration());
+            modelBuilder.ApplyConfiguration(new PartConfiguration());
+            modelBuilder.ApplyConfiguration(new ReleaseConfiguration());
+            modelBuilder.ApplyConfiguration(new TrackConfiguration());
         }
     }
 }
