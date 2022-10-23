e0f98e3054189d9e276598b96ac976112c229b2a
beresB <57649310+beresB@users.noreply.github.com>
2022-10-23 19:07:21 +0200

    XmlSerializer hozzáadva.

---
+++ OPM0PG_HFT_2022231.Repository/XmlSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace OPM0PG_HFT_2022231.Models
+{
+    class XmlSerializer<T>
+    {
+        XmlSerializer serializer;
+        public XmlSerializer()
+        {
+            serializer = new XmlSerializer(typeof(T));
+        }
+        public void Serialize(string path, T obj)
+        {
+            using (var writer = new StreamWriter(path, true))
+            {
+                serializer.Serialize(writer, obj);
+            }
+        }
+        public T Deserialize(string path)
+        {
+            using (var reader = new StreamReader(path, true))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
