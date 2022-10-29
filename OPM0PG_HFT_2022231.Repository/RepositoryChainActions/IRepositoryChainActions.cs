b78bbc654c747642c327ec6968b4556d8b5a106e
beresB <57649310+beresB@users.noreply.github.com>
2022-10-29 19:36:58 +0200

    ChainActions hozzáadva.

---
+++ OPM0PG_HFT_2022231.Repository/RepositoryChainActions/IRepositoryChainActions.cs
@@ -0,0 +1,20 @@
+﻿using OPM0PG_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OPM0PG_HFT_2022231.Repository.RepositoryChainActions
+{
+    public interface IRepositoryChainActions<TEntity> where TEntity : class, IEntity
+    {
+        IRepositoryChainActions<TEntity> CreateWithoutSave(IEnumerable<TEntity> entities);
+        IRepositoryChainActions<TEntity> CreateWithoutSave(params TEntity[] entities);
+        IRepositoryChainActions<TEntity> DeleteWithoutSave(IEnumerable<TEntity> entities);
+        IRepositoryChainActions<TEntity> DeleteWithoutSave(params TEntity[] entities);
+        IRepositoryChainActions<TEntity> Read(out TEntity entity, params object[] id);
+        IRepositoryChainActions<TEntity> ReadWhere(Func<TEntity, bool> predicate, out IEnumerable<TEntity> entities);
+        void Save();
+        IRepositoryChainActions<TEntity> UpdateWithoutSave(IEnumerable<TEntity> entities, Action<TEntity> updateAction);
+        IRepositoryChainActions<TEntity> UpdateWithoutSave(TEntity item, Action<TEntity> updateAction);
+        IRepositoryChainActions<TEntity> UpdateWithoutSave(TEntity entity);
+    }
+}
