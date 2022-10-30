c9e53d1c2128e11fc6ff36bdcb41df826a1867fe
beresB <57649310+beresB@users.noreply.github.com>
2022-10-30 02:04:36 +0200

    IOC beállítva.

--- OPM0PG_HFT_2022231.Endpoint/Startup.cs
+++ OPM0PG_HFT_2022231.Endpoint/Startup.cs
@@ -1,21 +1,46 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using OPM0PG_HFT_2022231.Logic.Implementations;
+using OPM0PG_HFT_2022231.Logic;
+using OPM0PG_HFT_2022231.Repository;
+
+using OPM0PG_HFT_2022231.Endpoint.JsonConverters;
 
 namespace OPM0PG_HFT_2022231.Endpoint
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<MusicDbContext>();
+            services.AddTransient<IMusicRepository, MusicRepository>();
+            services.AddTransient<IAlbumLogic, AlbumLogic>();
+            services.AddTransient<IArtistLogic, ArtistLogic>();
+            services.AddTransient<IContributionLogic, ContributionLogic>();
+            services.AddTransient<IGenreLogic, GenreLogic>();
+            services.AddTransient<IReleaseLogic, ReleaseLogic>();
+            services.AddTransient<IMusicLogic, MusicLogic>();
+
+            services.AddControllers().AddJsonOptions(config =>
+            {
+                config.JsonSerializerOptions.Converters.Add(new DurationJsonConverter());
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OPM0PG_HFT_2022231.Endpoint", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +49,16 @@ namespace OPM0PG_HFT_2022231.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OPM0PG_HFT_2022231.Endpoint v1"));
             }
 
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
