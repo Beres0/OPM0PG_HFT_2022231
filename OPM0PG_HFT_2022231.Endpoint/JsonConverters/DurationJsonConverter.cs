60140ad2e8faa49e2c3dc5e1f6da406ae6ab688b
beresB <57649310+beresB@users.noreply.github.com>
2022-10-30 02:00:50 +0200

    Timespan Converter,("hh:mm:ss"), hogy ne jelenjen meg az összes propertyje json-okban.

---
+++ OPM0PG_HFT_2022231.Endpoint/JsonConverters/DurationJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace OPM0PG_HFT_2022231.Endpoint.JsonConverters
+{
+
+    public class DurationJsonConverter : JsonConverter<TimeSpan>
+    {
+        string format = @"hh\:mm\:ss";
+
+        public DurationJsonConverter()
+        { }
+
+        public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (TimeSpan.TryParseExact(reader.GetString(), format, CultureInfo.CurrentCulture, out TimeSpan result))
+            {
+                return result;
+            }
+            return TimeSpan.Zero;
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(format));
+        }
+    }
+}
